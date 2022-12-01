67bdaa7055aa4dfeb9fc4780da4cb92dcd07b68f
magicaldave <smokydave@icloud.com>
2022-11-30 23:44:57 -0600

    Add: 3-dictionary_of_list_of_dictionaries.py, which gets all user's todo lists at once and JSONifies them.

---
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""This script gets information about a given employee's TODO list progress,
+and exports as JSON
+"""
+
+import json
+import requests
+
+if __name__ == "__main__":
+    # Request the employee ID.
+    employee_id = requests.get(
+        'https://jsonplaceholder.typicode.com/users/'
+    )
+    # Load the information sent back as variables
+    user_info = json.loads(employee_id.text)
+
+    tasks = {}
+    for user in user_info:
+        user_id = user['id']
+        todo_list = requests.get(
+            'https://jsonplaceholder.typicode.com/todos/?userId={}'
+            .format(user_id)
+        )
+        todo_info = json.loads(todo_list.text)
+    task_list = []
+    # Gather the information into a dictionary
+    for task in todo_info:
+        dictionary = {
+            'task': task['title'],
+            'completed': task['completed'],
+            'username': user_info['username']
+        }
+        task_list.append(dictionary)
+    # Create an array that represents the data to store in the table
+    tasks[USER_ID] = task_list
+    # Open the file to write to and write it as JSON
+    with open('todo_all_employees.json', 'w', encoding='UTF8',
+              newline='') as outfile:
+        outfile.write(json.dumps(tasks))
