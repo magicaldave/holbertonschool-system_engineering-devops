975f6276d31b273e8cde8135db4bd92e87ac299f
magicaldave <smokydave@icloud.com>
2022-11-30 23:57:23 -0600

    Edit: fixed sme variable names

--- 0x0E-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,11 +7,9 @@ import json
 import requests
 
 if __name__ == "__main__":
-    # Request the employee ID.
     employee_id = requests.get(
         'https://jsonplaceholder.typicode.com/users/'
     )
-    # Load the information sent back as variables
     user_info = json.loads(employee_id.text)
 
     tasks = {}
@@ -22,18 +20,18 @@ if __name__ == "__main__":
             .format(user_id)
         )
         todo_info = json.loads(todo_list.text)
+
         task_list = []
-        # Gather the information into a dictionary
         for task in todo_info:
             dictionary = {
-                'username': user_info['username'],
+                'username': user['username'],
                 'task': task['title'],
                 'completed': task['completed']
             }
             task_list.append(dictionary)
-    # Create an array that represents the data to store in the table
+
         tasks[user_id] = task_list
-    # Open the file to write to and write it as JSON
+
         with open('todo_all_employees.json', 'w', encoding='UTF8',
-                  newline='') as outfile:
-            outfile.write(json.dumps(tasks))
+                  newline='') as f:
+            f.write(json.dumps(tasks))
